224adc689f4c97ae19deeb15a80b18d1eb9ddc62
Sukrut Daphalapurkar <61420916+sukrutd@users.noreply.github.com>
2021-10-10 20:50:08 +0530

    Added Custom Redux Store Implementatiom

--- src/index.js
+++ src/index.js
@@ -106,3 +106,29 @@ boundIncrement();
 boundAdd(5);
 
 //============================================================================================================
+//========================================Redux Store Implementation==========================================
+
+/**
+ 
+const createStore = (reducer) => {
+	let state;
+	let listeners = [];
+
+	const getState = () => state;
+
+	const dispatch = (action) => {
+		state = reducer(state, action);
+
+		for (let i = 0; i < listeners.length; i++) {
+			listeners[i]();
+		}
+	};
+
+	const subscribe = (listener) => {
+		listeners.push(listener);
+	};
+
+	return { getState, dispatch, subscribe };
+};
+
+ */
